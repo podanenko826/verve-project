0f4a234408c8ec172c3642442c137dcb42cf1560
Denys Podanenko <podanenko826@icloud.com>
2023-11-30 19:06:14 +0200

    Made NavBar

---
+++ app/NavBar.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import React, { use } from 'react';
+import Link from 'next/link';
+
+import { usePathname } from 'next/navigation';
+
+import { FaRegCalendarCheck } from 'react-icons/fa';
+
+const NavBar = () => {
+  const navigation = [
+    { label: 'Dashboard', href: '/', id: 0 },
+    { label: 'Events', href: '/events', id: 1 },
+  ];
+
+  const currentPath = usePathname();
+
+  console.log(currentPath);
+
+  return (
+    <>
+      <nav className="flex min-w-full space-x-10 pl-10 bg-slate-100 p-6">
+        <Link className="text-2xl" href="/">
+          <FaRegCalendarCheck />
+        </Link>
+        <ul className="flex space-x-6">
+          {navigation.map((item) => (
+            <li key={item.id}>
+              <Link
+                href={item.href}
+                className={`${
+                  currentPath === item.href ? 'text-black' : 'text-zinc-500'
+                }`}
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
