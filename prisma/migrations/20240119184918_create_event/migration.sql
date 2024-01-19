9839abec1aa4516e659549be71655d17ee34aed4
Denys Podanenko <podanenko826@icloud.com>
2024-01-19 23:27:24 +0200

    Fixed event model

--- prisma/migrations/20240119184918_create_event/migration.sql
+++ prisma/migrations/20240119184918_create_event/migration.sql
@@ -2,8 +2,8 @@
 CREATE TABLE `Event` (
     `event_id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(255) NOT NULL,
-    `start` DATETIME(3) NOT NULL,
-    `end` DATETIME(3) NOT NULL,
+    `start` VARCHAR(255) NOT NULL,
+    `end` VARCHAR(255) NOT NULL,
     `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN',
 
     PRIMARY KEY (`event_id`)
