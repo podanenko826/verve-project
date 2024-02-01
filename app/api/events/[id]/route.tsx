143a66f527b0fbcb3b3acc780505e386f175cec2
Denys Podanenko <podanenko826@icloud.com>
2024-02-01 17:33:39 +0200

    Create dynamic route

--- app/api/events/[id]/route.tsx
+++ app/api/events/[id]/route.tsx
@@ -1,24 +1,25 @@
-import { NextResponse } from 'next/server';
-import React from 'react';
+import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-interface Props {
-  params: { id: number };
-}
-
-export const get = async ({ params: { id } }: Props) => {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     const event = await prisma.event.findUnique({
       where: {
-        event_id: id,
+        event_id: parseInt(params.id),
       },
     });
 
     if (!event) {
       return NextResponse.json({ error: 'Event not found' }, { status: 404 });
     }
+
+    // Convert the Prisma model instance to a plain JavaScript object
+    // const parsedEvent = JSON.parse(JSON.stringify(event));
 
     return NextResponse.json(event);
   } catch (error) {
@@ -29,4 +30,4 @@ export const get = async ({ params: { id } }: Props) => {
       { status: 500 }
     );
   }
-};
+}
