60f070972eaca6ebb4e7fb39160a7d4bd18e3b8c
Denys Podanenko <podanenko826@icloud.com>
2024-02-02 09:48:36 +0200

    Create refetch data button

--- app/Scheduler.tsx
+++ app/Scheduler.tsx
@@ -57,17 +57,21 @@ const ISheduller = () => {
     }
   };
 
-  const handleConfirm = async (event: ProcessedEvent, action: EventActions) => {
-    let returnedEvent: ProcessedEvent = {
-      ...event,
-    };
+  async function updateEventOnServer(updatedId: any) {
+    try {
+      const response = await axios.put(`/api/events/${updatedId}`);
 
-    if (action === 'create') {
-      await addEventOnServer(returnedEvent);
+      if (response.status === 200) {
+        return true;
+      } else {
+        console.error('Unexpected response status:', response.status);
+        return false;
+      }
+    } catch (error) {
+      console.error('Error updating event:', error);
+      throw error;
     }
-
-    return returnedEvent;
-  };
+  }
 
   async function deleteEventOnServer(deletedId: any) {
     try {
@@ -85,32 +89,41 @@ const ISheduller = () => {
     }
   }
 
+  const handleConfirm = async (event: ProcessedEvent, action: EventActions) => {
+    let returnedEvent: ProcessedEvent = {
+      ...event,
+    };
+
+    if (action === 'edit') {
+      returnedEvent = event;
+      await updateEventOnServer(returnedEvent);
+    }
+    if (action === 'create') {
+      await addEventOnServer(returnedEvent);
+    }
+
+    return returnedEvent;
+  };
+
+  const handleUpdate = async (updatedId: any) => {
+    await updateEventOnServer(updatedId);
+
+    return updatedId;
+  };
+
   const handleDelete = async (deletedId: any) => {
     await deleteEventOnServer(deletedId);
 
     return deletedId;
   };
 
-  async function updateEventOnServer(updatedId: any) {
+  const refetchData = async () => {
     try {
-      const response = await axios.put(`/api/events/${updatedId}`);
-
-      if (response.status === 200) {
-        return true;
-      } else {
-        console.error('Unexpected response status:', response.status);
-        return false;
-      }
+      const response = await axios.get('/api/events');
+      setEvents(response.data);
     } catch (error) {
-      console.error('Error updating event:', error);
-      throw error;
+      console.error('Error fetching event:', error);
     }
-  }
-
-  const handleUpdate = async (updatedId: any) => {
-    await updateEventOnServer(updatedId);
-
-    return updatedId;
   };
 
   return (
@@ -131,6 +144,13 @@ const ISheduller = () => {
           onEventDrop={handleUpdate}
         />
       )}
+
+      <button
+        className="w-32 m-5 h-10 bg-slate-400 hover:bg-slate-300 active:bg-slate-500 border-2"
+        onClick={refetchData}
+      >
+        Refresh data
+      </button>
     </div>
   );
 };
--- app/SideBar.tsx
+++ app/SideBar.tsx
@@ -4,9 +4,9 @@ import { AiFillCarryOut } from 'react-icons/ai';
 
 const SideBar = () => {
   return (
-    <div className="flex flex-col w-[100px] items-center min-h-full bg-gray-300 z-50 hover:w-[180px] transition-all duration-300">
+    <div className="flex w-[100px] items-center min-h-screen bg-gray-300 z-50 hover:w-[180px] transition-all hidden md:block duration-300">
       <Link href="/">
-        <AiFillCarryOut className="text-3xl mt-6" />
+        <AiFillCarryOut className="text-[55px] pt-6" />
       </Link>
     </div>
   );
