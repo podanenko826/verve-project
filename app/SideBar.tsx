b5ad061adaf537ad14415ce8b05ff0d1f40f1544
Denys Podanenko <podanenko826@icloud.com>
2024-02-02 14:27:23 +0200

    Create sign in and route protect

--- app/SideBar.tsx
+++ app/SideBar.tsx
@@ -5,6 +5,7 @@ import { AiFillCarryOut } from 'react-icons/ai';
 import { usePathname } from 'next/navigation';
 
 import { FaFacebook, FaInstagramSquare } from 'react-icons/fa';
+import { useSession } from 'next-auth/react';
 
 const SideBar = () => {
   const navigation = [
@@ -14,6 +15,8 @@ const SideBar = () => {
   ];
 
   const currentPath = usePathname();
+
+  const { status, data: session } = useSession();
 
   return (
     <nav className="w-[170px] min-h-screen space-y-10 shadow-2xl z-50 transition-all hidden md:block duration-300">
@@ -30,12 +33,35 @@ const SideBar = () => {
                 currentPath === item.href
                   ? 'text-black'
                   : 'text-zinc-500 hover:text-zinc-700'
-              } transition-all bg-white px-7 py-1 rounded-lg shadow-lg hover:shadow-sm duration-200 active:shadow-lg`}
+              } transition-all bg-white px-7 py-1 font-semibold rounded-lg shadow-lg hover:shadow-sm duration-200 active:shadow-lg`}
             >
               {item.label}
             </Link>
           </li>
         ))}
+        <li>
+          {status === 'unauthenticated' && (
+            <div className="pt-10">
+              <Link
+                className="transition-all bg-white px-10 py-1 font-semibold rounded-lg shadow-lg hover:shadow-sm duration-200 active:shadow-lg"
+                href="/api/auth/signin"
+              >
+                Sign in
+              </Link>
+            </div>
+          )}
+          {status === 'authenticated' && (
+            <div className="flex flex-col space-y-2 pt-10">
+              <p className="font-semibold">{session.user?.name}</p>
+              <Link
+                className="transition-all bg-white py-1 font-semibold rounded-lg shadow-lg hover:shadow-sm duration-200 active:shadow-lg"
+                href="/api/auth/signout"
+              >
+                Sign out
+              </Link>
+            </div>
+          )}
+        </li>
       </ul>
       <ul className="flex text-3xl justify-center w-40 space-x-2 ">
         <li>
