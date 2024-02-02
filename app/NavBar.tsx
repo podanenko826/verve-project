8035735d0c7980fe898ae90f0cc89b32c66af676
Denys Podanenko <podanenko826@icloud.com>
2024-02-02 12:02:05 +0200

    Create search bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,52 +8,25 @@ import { usePathname } from 'next/navigation';
 // import { FcCalendar } from 'react-icons/fc';
 import { FaFacebook, FaInstagramSquare } from 'react-icons/fa';
 import { AiFillCarryOut } from 'react-icons/ai';
+import { IoMdSearch } from 'react-icons/io';
 
 const NavBar = () => {
-  const navigation = [
-    { label: 'Dashboard', href: '/', id: 0 },
-    { label: 'Events', href: '/Events', id: 1 },
-    { label: 'Login', href: '/api/auth/signin', id: 2 },
-  ];
-  const currentPath = usePathname();
-
-  console.log(currentPath);
-
   return (
     <>
       <nav className="flex justify-around items-center min-w-full h-20">
-        <ul className="flex space-x-6">
-          {navigation.map((item) => (
-            <li key={item.id}>
-              <Link
-                href={item.href}
-                className={`${
-                  currentPath === item.href
-                    ? 'text-black'
-                    : 'text-zinc-500 hover:text-zinc-700'
-                } transition-colors`}
-              >
-                {item.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <Link className="flex text-3xl" href="/">
-          <AiFillCarryOut className="mt-0.5 pt-0.5" />
-          <h1 className="font-light">Verve</h1>
-        </Link>
-        <ul className="flex text-3xl justify-center w-40 space-x-2">
-          <li>
-            <Link href="#">
-              <FaInstagramSquare />
-            </Link>
-          </li>
-          <li>
-            <Link href="#">
-              <FaFacebook />
-            </Link>
-          </li>
-        </ul>
+        <form className="flex">
+          <label htmlFor="search">
+            <IoMdSearch className="text-3xl fill-slate-400 absolute pl-2 z-50" />
+          </label>
+          <input
+            type="search"
+            id="search"
+            name="q"
+            placeholder=""
+            className="rounded-xl w-96 pl-10 p-0.5 shadow-lg hover:shadow-md transition-all active:shadow-lg active:scale-y-105 duration-300 font-semibold outline-none active:border-2"
+          />
+          {/* <button type="submit">Search</button> */}
+        </form>
       </nav>
     </>
   );
