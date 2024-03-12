c681c2b8a7951bae92ca7f6b11844a6e1bf4fc58
Denys Podanenko <podanenko826@icloud.com>
2024-03-12 18:26:02 +0200

    Improved Dynamic Search and updateEventOnServer

--- app/DynamicSearch.tsx
+++ app/DynamicSearch.tsx
@@ -1,28 +1,73 @@
+'use client';
 import React, { useEffect, useState } from 'react';
 import { IoMdSearch } from 'react-icons/io';
 import axios from 'axios';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
+import { enUS } from 'date-fns/locale';
+import { format } from 'date-fns';
+import Link from 'next/link';
 
-interface DynamicSearchProps {
-  onSearch: (query: string) => void;
-  events?: any;
+enum Status {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  CLOSED = 'CLOSED',
+}
+
+interface Event {
+  event_id: number;
+  title: string;
+  start: Date | number;
+  end: Date | number;
+  status: Status;
 }
 
 const DynamicSearch = () => {
   const [data, setData] = useState('');
-  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [selectedEvent, setSelectedEvent] = useState<Event>();
   const [eventTitle, setEventTitle] = useState('');
   const [isSearchBarEmpty, setIsSearchBarEmpty] = useState(true);
+  const [events, setEvents] = useState<Event[]>([]);
 
-  const getEvents = async () => {
-    try {
-      const response = await axios.get('api/events');
+  const router = useRouter();
 
-      if (response) return response;
-    } catch (error) {
-      console.log('Error fetching events:' + error);
+  useEffect(() => {
+    const fetchEvent = async () => {
+      try {
+        const response = await axios.get('/api/events');
+
+        setEvents(response.data);
+      } catch (error) {
+        console.error('Error fetching event:', error);
+      }
+    };
+
+    fetchEvent();
+  }, []);
+
+  async function deleteEventOnServer(
+    event: React.MouseEvent<HTMLButtonElement>,
+    deletedId: any
+  ) {
+    event.preventDefault();
+
+    if (!deletedId) {
+      return console.error('Id is required to delete an event.');
     }
-  };
+
+    try {
+      const response = await axios.delete(`/api/events/${deletedId}`);
+
+      if (response.status === 200) {
+        return true; // Event deleted successfully
+      } else {
+        console.error('Unexpected response status:', response.status);
+        return false; // Unexpected response status
+      }
+    } catch (error) {
+      console.error('Error deleting event:', error);
+      throw error; // Propagate the error or handle it as needed
+    }
+  }
 
   const handleInput = async (input: string) => {
     setData(input);
@@ -32,6 +77,7 @@ const DynamicSearch = () => {
 
     try {
       const response = await axios.get('/api/events');
+      setEvents(response.data);
 
       for (let i = 0; i < response.data.length; i++) {
         let e = response.data[i];
@@ -41,7 +87,7 @@ const DynamicSearch = () => {
           setEventTitle(e.title);
           break;
         } else {
-          setSelectedEvent(null);
+          setSelectedEvent(undefined);
           setEventTitle('');
         }
       }
@@ -50,9 +96,54 @@ const DynamicSearch = () => {
     }
   };
 
-  useEffect(() => {
-    console.log('Event:', selectedEvent);
-  }, [selectedEvent]);
+  const handleEventChoose = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    selectedEventId: number
+  ) => {
+    event.preventDefault();
+
+    for (let i = 0; i < events.length; i++) {
+      if (events[i].event_id === selectedEventId) {
+        setData(events[i].title);
+        setEventTitle(events[i].title);
+        setSelectedEvent(events[i]);
+      }
+    }
+  };
+
+  const handleEdit = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    router.push('/events/edit/' + selectedEvent?.event_id);
+  };
+
+  let startDate,
+    startTime: string | null = null;
+
+  if (selectedEvent && selectedEvent.start !== undefined) {
+    const startDateTime = new Date(selectedEvent.start);
+    startDate = format(startDateTime, 'd MMMM yyyy', {
+      locale: enUS,
+    });
+    startTime = format(startDateTime, 'h:mm a', {
+      // HH:mm (without a) for European time
+      locale: enUS,
+    });
+  }
+
+  let endDate,
+    endTime: string | null = null;
+
+  if (selectedEvent && selectedEvent.end !== undefined) {
+    const endDateTime = new Date(selectedEvent.end);
+    endDate = format(endDateTime, 'd MMMM yyyy', {
+      locale: enUS,
+    });
+    endTime = format(endDateTime, 'h:mm a', {
+      // HH:mm (without a) for European time
+      locale: enUS,
+    });
+  }
 
   return (
     <div>
@@ -69,13 +160,13 @@ const DynamicSearch = () => {
             onChange={(e) => handleInput(e.target.value)}
             className={`${
               selectedEvent || data.toLowerCase() === 'event'
-                ? 'bg-transparent'
-                : 'shadow-lg active:border-2 hover:shadow-md active:shadow-lg dark:bg-gray-800'
-            } rounded-xl md:w-72 text-top lg:w-96 pl-10 p-0.5 custom-z-index-great outline-none ease-in-out active:scale-y-105 transition-all duration-300 font-semibold`}
+                ? 'bg-white rounded-t-xl'
+                : 'shadow-lg active:border-2 hover:shadow-md active:shadow-lg dark:bg-gray-800 rounded-xl'
+            } md:w-72 text-top max-w-96 pl-10 p-0.5 custom-z-index-great outline-none ease-in-out active:scale-y-105 transition-all duration-500 font-semibold`}
           />
 
           <button
-            className={`custom-z-index-great ml-[10px] px-3 bg-slate-50 hover:bg-slate-100 hover:border active:bg-slate-200 dark:bg-gray-700 duration-300 transition-all rounded-lg shadow-lg hover:shadow-xl font-light text-slate-600 dark:text-slate-300`}
+            className={`custom-z-index-great ml-[10px] px-3 bg-slate-50 hover:bg-slate-100 hover:border active:bg-slate-200 dark:bg-gray-700 duration-500 transition-all rounded-lg shadow-lg hover:shadow-xl font-light text-slate-600 dark:text-slate-300`}
             type="submit"
           >
             Search
@@ -85,18 +176,54 @@ const DynamicSearch = () => {
         <div
           className={`${
             selectedEvent || data.toLowerCase() === 'event'
-              ? 'dynamic-search shadow-lg active:border-2 hover:shadow-md active:shadow-lg bg-white dark:bg-gray-800'
-              : 'dynamic-search-reverse bg-transparent'
-          } flex flex-col pt-8 justify-around items-center rounded-xl absolute md:w-72 text-top lg:w-96 p-0.5 custom-z-index ease-in-out active:scale-y-105 transition-all duration-300 font-semibold outline-none`}
+              ? 'dynamic-search-event-action shadow-lg active:border-2 hover:shadow-md active:shadow-lg bg-white dark:bg-gray-800'
+              : 'dynamic-search-event-actions-revers bg-transparent'
+          } overflow-x-auto flex flex-col pt-[26px] justify-around items-center rounded-xl absolute w-80 max-h-[700px] md:w-72 text-top lg:w-96 p-0.5 custom-z-index ease-in-out active:scale-y-105 transition-all duration-500 font-semibold outline-none`}
         >
-          <p className="font-mono">{selectedEvent ? eventTitle : ''}</p>
-
           {/* Code to display searched event delete and modify buttons */}
 
           {selectedEvent ? (
-            <button className="custom-z-index-great px-3 py-3 bg-zinc-100 rounded-lg shadow-xl hover:shadow-xl font-light text-slate-600">
-              Delete
-            </button>
+            <>
+              <div className="flex flex-col items-center w-full">
+                <p className="font-mono">{selectedEvent ? eventTitle : ''}</p>
+                {startDate === endDate ? (
+                  <p className="font-normal text-sm text-center px-10">
+                    {startDate}, {startTime} - {endTime}
+                  </p>
+                ) : (
+                  <p className="font-normal text-[16px] text-center px-6 lg:px-4">
+                    From {startDate} to {endDate}, <br />
+                    {startTime} - {endTime}
+                  </p>
+                )}
+              </div>
+              <div className="my-5 space-x-5">
+                <button
+                  onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+                    handleEdit(e)
+                  }
+                  className="custom-z-index-great px-3 py-3 bg-zinc-100 hover:bg-zinc-200 rounded-lg shadow-xl transition-all hover:shadow-xl font-light text-slate-600"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+                    deleteEventOnServer(e, selectedEvent.event_id)
+                  }
+                  className="custom-z-index-great px-3 py-3 bg-zinc-100 hover:bg-zinc-200 rounded-lg shadow-xl transition-all hover:shadow-xl font-light text-slate-600"
+                >
+                  Delete
+                </button>
+                <button
+                  // onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+                  //   handleButtonClick(e)
+                  // }
+                  className="custom-z-index-great px-3 py-3 bg-zinc-100 hover:bg-zinc-200 rounded-lg shadow-xl transition-all hover:shadow-xl font-light text-slate-600"
+                >
+                  Archive
+                </button>
+              </div>
+            </>
           ) : (
             ''
           )}
@@ -104,8 +231,20 @@ const DynamicSearch = () => {
           {/* Code to display all events in a search bar */}
 
           {data.toLowerCase() === 'event' ? (
-            <ul>
-              <li>hi</li>
+            <ul className="mt-[2px]">
+              {events.map((item) => (
+                <li key={item.event_id}>
+                  <button
+                    onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+                      handleEventChoose(e, item.event_id)
+                    }
+                    key={item.event_id}
+                    className="w-[315px] md:w-screen text-center hover:bg-slate-100 font-normal py-[6px] border-y border-zinc-100 duration-300 transition-all"
+                  >
+                    {item.title}
+                  </button>
+                </li>
+              ))}
             </ul>
           ) : (
             ''
