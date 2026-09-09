a712108d6f3668c2ff323a55c06770b6e9d207fe
Denis Podanenko <podanenko826@gmail.com>
2026-09-09 13:47:28 +0200

    UI improvements

--- app/SideBar.tsx
+++ app/SideBar.tsx
@@ -15,100 +15,99 @@ import { FaHeart } from 'react-icons/fa6';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 
 const SideBar = () => {
-  const navigation = [
-    {
-      icon: <RiDashboard2Line />,
-      label: 'Dashboard',
-      href: '/dashboard',
-      id: 0,
-    },
-    {
-      icon: <RiCalendarEventLine />,
-      label: 'Events',
-      href: '/events',
-      id: 1,
-    },
-    {
-      icon: <IoSettingsOutline />,
-      label: 'Settings',
-      href: '/settings',
-      id: 2,
-    },
-    {
-      icon: <MdOutlineAccountCircle />,
-      label: 'Account',
-      href: '/account',
-      id: 3,
-    },
-  ];
+    const navigation = [
+        {
+            icon: <RiDashboard2Line />,
+            label: 'Dashboard',
+            href: '/dashboard',
+            id: 0,
+        },
+        {
+            icon: <RiCalendarEventLine />,
+            label: 'Events',
+            href: '/events',
+            id: 1,
+        },
+        {
+            icon: <IoSettingsOutline />,
+            label: 'Settings',
+            href: '/settings',
+            id: 2,
+        },
+        {
+            icon: <MdOutlineAccountCircle />,
+            label: 'Account',
+            href: '/account',
+            id: 3,
+        },
+    ];
 
-  const currentPath = usePathname();
+    const currentPath = usePathname();
 
-  const { status, data: session } = useSession();
+    const { status, data: session } = useSession();
 
-  const [selectedOption, setSelectedOption] = useState('option1');
+    const [selectedOption, setSelectedOption] = useState('option1');
 
-  const handleOptionChange = (event: any) => {
-    setSelectedOption(event.target.value);
-  };
+    const handleOptionChange = (event: any) => {
+        setSelectedOption(event.target.value);
+    };
 
-  return (
-    <nav className="flex flex-col justify-between albertsans bg-gradient-sidebar dark:bg-gradient-sidebar-dark pb-10 w-[240px] min-h-screen space-y-10 shadow-2xl z-50 transition-all duration-300 overflow-hidden">
-      <div>
-        <div className="space-y-5 py-7 flex flex-col items-center justify-between">
-          <Link className="flex text-3xl pr-20 ml-2 text-white" href="/">
-            <AiFillCarryOut className="mt-0.5 pt-0.5" />
-            <h1 className="albertsans font-bold hidden md:block">Verve</h1>
-          </Link>
+    return (
+        <nav className="flex flex-col justify-between albertsans bg-gradient-sidebar dark:bg-gradient-sidebar-dark pb-10 w-[260px] min-h-screen space-y-10 shadow-2xl z-50 transition-all duration-300 overflow-hidden">
+            <div>
+                <div className="space-y-5 p-6 flex flex-col">
+                    <Link className="flex text-3xl text-white" href="/">
+                        <AiFillCarryOut className="mt-0.5 pt-0.5" />
+                        <h1 className="albertsans font-bold">Verve</h1>
+                    </Link>
 
-          <div className="w-5/6 rounded-2xl border border-zinc-400">
-            <h3 className="pl-3 py-0.5 text-zinc-300 text-[15px]">Workspace</h3>
-            <select
-              className="w-[190px] text-white bg-transparent outline-none pl-2 pb-1"
-              value={selectedOption}
-              onChange={handleOptionChange}
-            >
-              <option value="option1">Personal</option>
-              <option value="option2">Business</option>
-            </select>
-          </div>
-        </div>
-        <ul className="flex md:flex-col border-t-[1.5px] border-gray-400 pt-10 pb-28 space-y-4 text-center">
-          {navigation.map((item) => (
-            <li key={item.id}>
-              <Link
-                href={item.href}
-                className={`${
-                  currentPath === item.href
-                    ? 'text-gray-200 dark:bg-blue-400 shadow-shadow-sidebar'
-                    : 'text-gray-400 hover:text-gray-200'
-                } rounded-lg transition-all ml-3 w-5/6 px-4 py-1.5 font-bold flex items-center text-[16px] duration-200`}
-              >
-                <strong className="pr-1.5">{item.icon}</strong>
-                {item.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+                    <div className="rounded-2xl border border-zinc-400 px-3 py-0.5">
+                        <h3 className="py-0.5 text-zinc-300 text-[15px]">Workspace</h3>
+                        <select
+                            className="w-full text-white bg-transparent outline-none pb-1"
+                            value={selectedOption}
+                            onChange={handleOptionChange}
+                        >
+                            <option value="option1">Personal</option>
+                            <option value="option2">Business</option>
+                        </select>
+                    </div>
+                </div>
+                <ul className="flex md:flex-col border-t-[1px] border-gray-400 pt-10 pb-28 space-y-4 text-center">
+                    {navigation.map((item) => (
+                        <li key={item.id}>
+                            <Link
+                                href={item.href}
+                                className={`${currentPath === item.href
+                                    ? 'text-gray-200 dark:bg-blue-400 shadow-shadow-sidebar'
+                                    : 'text-gray-400 hover:text-gray-200'
+                                    } transition-all px-6 py-3 flex items-center text-[16px] duration-200`}
+                            >
+                                <strong className="pr-1.5">{item.icon}</strong>
+                                {item.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
 
-      <div className="flex flex-col justify-end items-center w-full h-min">
-        <div className="flex text-gray-200">
-          <h1>Made with</h1>
-          <FaHeart className="text-red-600 mt-1 ml-1.5" />
-          <h1 className="ml-1.5">by</h1>
-        </div>
-        <Link
-          href={'https://twitter.com/denys_podanenko'}
-          target="_blank"
-          className="flex text-gray-200"
-        >
-          <h1>Denys Podanenko</h1>
-          <FaExternalLinkAlt className="mt-1 ml-1.5" />
-        </Link>
-      </div>
-    </nav>
-  );
+            <div className="flex flex-col justify-end items-center w-full h-min">
+                <div className="flex text-gray-200">
+                    <h1>Made with</h1>
+                    <FaHeart className="text-red-600 mt-1 ml-1.5" />
+                    <h1 className="ml-1.5">by</h1>
+                </div>
+                <Link
+                    href={'https://twitter.com/denys_podanenko'}
+                    target="_blank"
+                    className="flex text-gray-200"
+                >
+                    <h1>Denys Podanenko</h1>
+                    <FaExternalLinkAlt className="mt-1 ml-1.5" />
+                </Link>
+            </div>
+        </nav>
+    );
 };
 
 export default SideBar;
--- app/account/page.tsx
+++ app/account/page.tsx
@@ -8,51 +8,53 @@ import { useSession } from 'next-auth/react';
 import { FaUserCircle } from 'react-icons/fa';
 
 export default async function AccountPage() {
-  const prisma = new PrismaClient();
-  const session = await getServerSession();
+    const prisma = new PrismaClient();
+    const session = await getServerSession();
 
-  const currentUser = await prisma.user.findUnique({
-    where: {
-      email: session?.user.email!,
-    },
-  });
-  // const currentUser = allUsers.filter;
+    const currentUser = await prisma.user.findUnique({
+        where: {
+            email: session?.user.email!,
+        },
+    });
+    // const currentUser = allUsers.filter;
 
-  return (
-    <>
-      <div className="flex w-full">
-        <div className="hidden md:block h-full">
-          <SideBar />
-        </div>
-        <div className="pl-6 flex-col w-full">
-          <NavBar />
-          <p className="text-3xl">Account</p>
-          <div className="flex flex-col items-center justify-around mt-8 bg-white w-[450px] border-[1.5px] rounded-[25px]">
-            <div className="flex flex-col items-center">
-              {currentUser?.image ? (
-                <img
-                  src={currentUser?.image!}
-                  referrerPolicy="no-referrer"
-                  alt=""
-                  className="w-20 h-20 mt-6 rounded-full"
-                  draggable={false}
-                />
-              ) : (
-                <FaUserCircle className="text-[70px]" />
-              )}
-              <p className="text-2xl mt-4">{currentUser?.name}</p>
-              <p className="text-lg mt-4 text-slate-400">Account ID:</p>
-              <p className="text-sm text-slate-500">{currentUser?.id}</p>
+    return (
+        <>
+            <div className="flex w-full">
+                <div className="hidden md:block h-full">
+                    <SideBar />
+                </div>
+                <div className="flex flex-col w-full">
+                    <NavBar />
+                    <div className="flex flex-col mx-16">
+                        <p className="text-3xl">Account</p>
+                        <div className="flex flex-col items-center justify-around mt-8 bg-white w-[450px] border-[1.5px] rounded-[25px]">
+                            <div className="flex flex-col items-center">
+                                {currentUser?.image ? (
+                                    <img
+                                        src={currentUser?.image!}
+                                        referrerPolicy="no-referrer"
+                                        alt=""
+                                        className="w-20 h-20 mt-6 rounded-full"
+                                        draggable={false}
+                                    />
+                                ) : (
+                                    <FaUserCircle className="text-[70px]" />
+                                )}
+                                <p className="text-2xl mt-4">{currentUser?.name}</p>
+                                <p className="text-lg mt-4 text-slate-400">Account ID:</p>
+                                <p className="text-sm text-slate-500">{currentUser?.id}</p>
+                            </div>
+                            <div className="flex justify-center border-t-[1.5px] w-full mt-5 overflow-hidden">
+                                <button className="w-full py-5 rounded-b-[25px] text-blue-500 active:text-blue-300 hover:bg-gray-50 duration-100 transition-all">
+                                    Upload picture
+                                </button>
+                                <input type="file" className="upload-picture hidden" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="flex justify-center border-t-[1.5px] w-full mt-5 overflow-hidden">
-              <button className="w-full py-5 rounded-b-[25px] text-blue-500 active:text-blue-300 hover:bg-gray-50 duration-100 transition-all">
-                Upload picture
-              </button>
-              <input type="file" className="upload-picture hidden" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
